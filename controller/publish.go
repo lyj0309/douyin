dd2f092ebbd9049dea9a71b0971234c9b4e9efb5
liyajunwork@163.com <1491085293@qq.com>
2022-05-29 10:46:56 +0800

    build dev yajun

--- controller/publish.go
+++ controller/publish.go
@@ -2,24 +2,42 @@ package controller
 
 import (
 	"fmt"
-	"github.com/gin-gonic/gin"
 	"net/http"
 	"path/filepath"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/lyj0309/douyin/db"
 )
 
 type VideoListResponse struct {
 	Response
-	VideoList []Video `json:"video_list"`
+	VideoList []db.Video `json:"video_list"`
 }
 
 // Publish check token then save upload file to public directory
 func Publish(c *gin.Context) {
-	token := c.PostForm("token")
+	// token := c.PostForm("token")
 
-	if _, exist := usersLoginInfo[token]; !exist {
-		c.JSON(http.StatusOK, Response{StatusCode: 1, StatusMsg: "User doesn't exist"})
+	// NEW
+	var user db.User
+
+	//需要判断该用户名是否被占用
+	res := db.Mysql.Where("name = ?", user.Name).Find(&user)
+	if res.RowsAffected == 0 {
+		c.JSON(http.StatusOK, gin.H{
+			"code": 1,
+			"msg":  "User doesn't exist",
+		})
 		return
 	}
+
+	// NEW END
+
+	// if _, exist := usersLoginInfo[token]; !exist {
+	// 	c.JSON(http.StatusOK, Response{StatusCode: 1, StatusMsg: "User doesn't exist"})
+	// 	return
+	// }
 
 	data, err := c.FormFile("data")
 	if err != nil {
@@ -31,8 +49,8 @@ func Publish(c *gin.Context) {
 	}
 
 	filename := filepath.Base(data.Filename)
-	user := usersLoginInfo[token]
-	finalName := fmt.Sprintf("%d_%s", user.Id, filename)
+	// user := usersLoginInfo[token]
+	finalName := fmt.Sprintf("%d_%s", user.ID, filename)
 	saveFile := filepath.Join("./public/", finalName)
 	if err := c.SaveUploadedFile(data, saveFile); err != nil {
 		c.JSON(http.StatusOK, Response{
@@ -42,6 +60,20 @@ func Publish(c *gin.Context) {
 		return
 	}
 
+	var video db.Video
+	res_video := db.Mysql.Find(&video)
+	newVideo := db.Video{
+		ID:         uint(res_video.RowsAffected) + 1,
+		Title:      "视频标题", //视频标题
+		UserID:     user.ID,
+		PlayUrl:    "http://10.113.166.143:8080/static/" + finalName,                  //视频播放地址
+		CoverUrl:   "http://10.113.166.143:8080/static/20190330000110_360x480_55.jpg", //视频封面地址
+		CreateTime: time.Now(),                                                        //创建时间
+		// FavoriteCount: 0,
+		// CommentCount:  0,
+	}
+	db.Mysql.Create(&newVideo)
+
 	c.JSON(http.StatusOK, Response{
 		StatusCode: 0,
 		StatusMsg:  finalName + " uploaded successfully",
@@ -50,10 +82,13 @@ func Publish(c *gin.Context) {
 
 // PublishList all users have same publish video list
 func PublishList(c *gin.Context) {
+	var video []db.Video
+	db.Mysql.Find(&video)
+	fmt.Print(video)
 	c.JSON(http.StatusOK, VideoListResponse{
 		Response: Response{
 			StatusCode: 0,
 		},
-		VideoList: DemoVideos,
+		VideoList: video,
 	})
 }
