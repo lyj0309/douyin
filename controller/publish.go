912a8abdd313944ae486ee200503e8206d702a11
liyajunwork@163.com <1491085293@qq.com>
2022-05-31 09:49:23 +0800

    11

--- controller/publish.go
+++ controller/publish.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"net/http"
 	"path/filepath"
-	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/lyj0309/douyin/db"
@@ -17,13 +16,13 @@ type VideoListResponse struct {
 
 // Publish check token then save upload file to public directory
 func Publish(c *gin.Context) {
-	// token := c.PostForm("token")
+
+	userIdStr := c.GetString(ctxUidKey)
 
 	// NEW
 	var user db.User
 
-	//需要判断该用户名是否被占用
-	res := db.Mysql.Where("name = ?", user.Name).Find(&user)
+	res := db.Mysql.Where("id = ?", userIdStr).Find(&user)
 	if res.RowsAffected == 0 {
 		c.JSON(http.StatusOK, gin.H{
 			"code": 1,
@@ -38,7 +37,7 @@ func Publish(c *gin.Context) {
 	// 	c.JSON(http.StatusOK, Response{StatusCode: 1, StatusMsg: "User doesn't exist"})
 	// 	return
 	// }
-
+	title := c.PostForm("title")
 	data, err := c.FormFile("data")
 	if err != nil {
 		c.JSON(http.StatusOK, Response{
@@ -60,19 +59,19 @@ func Publish(c *gin.Context) {
 		return
 	}
 
-	var video db.Video
-	res_video := db.Mysql.Find(&video)
-	newVideo := db.Video{
-		ID:         uint(res_video.RowsAffected) + 1,
-		Title:      "视频标题", //视频标题
-		UserID:     user.ID,
-		PlayUrl:    "http://10.113.166.143:8080/static/" + finalName,                  //视频播放地址
-		CoverUrl:   "http://10.113.166.143:8080/static/20190330000110_360x480_55.jpg", //视频封面地址
-		CreateTime: time.Now(),                                                        //创建时间
-		// FavoriteCount: 0,
-		// CommentCount:  0,
-	}
-	db.Mysql.Create(&newVideo)
+	// service.videoAdd(userId, videoId, commentText)
+
+	// newVideo := db.Video{
+
+	// 	Title:      title, //视频标题
+	// 	UserID:     user.ID,
+	// 	PlayUrl:    "http://10.113.166.143:8080/static/" + finalName,                  //视频播放地址
+	// 	CoverUrl:   "http://10.113.166.143:8080/static/20190330000110_360x480_55.jpg", //视频封面地址
+	// 	CreateTime: time.Now(),                                                        //创建时间
+	// 	// FavoriteCount: 0,
+	// 	// CommentCount:  0,
+	// }
+	// db.Mysql.Save(&newVideo)
 
 	c.JSON(http.StatusOK, Response{
 		StatusCode: 0,
@@ -84,7 +83,7 @@ func Publish(c *gin.Context) {
 func PublishList(c *gin.Context) {
 	var video []db.Video
 	db.Mysql.Find(&video)
-	fmt.Print(video)
+	// fmt.Print(video)
 	c.JSON(http.StatusOK, VideoListResponse{
 		Response: Response{
 			StatusCode: 0,
