ab7401fe3c382b8a9ca81ad79550881cefe39d8e
Jiaoyang <1243763027@qq.com>
2022-06-03 21:10:25 +0800

    favorite part

--- controller/favorite.go
+++ controller/favorite.go
@@ -2,14 +2,20 @@ package controller
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/lyj0309/douyin/service"
 	"net/http"
 	"strconv"
 )
 
+type FavoriteListResponse struct {
+	Response
+	VideoList []service.FavoriteVideo `json:"video_list,omitempty"`
+}
+
 // FavoriteAction no practical effect, just check if token is valid
 func FavoriteAction(c *gin.Context) {
 	userIdStr := c.PostForm("user_id")
-	token := c.PostForm("token")
+	// userIdStr := c.GetString(ctxUidKey)
 	videoIdStr := c.PostForm("video_id")
 	actionTypeStr := c.PostForm("action_type")
 	userId, _ := strconv.ParseInt(userIdStr, 10, 64)
@@ -17,29 +23,34 @@ func FavoriteAction(c *gin.Context) {
 	actionType, _ := strconv.ParseInt(actionTypeStr, 10, 8)
 
 	// 验证用户
-	if uid, err := JwtParseUser(token); err != nil {
-		c.JSON(http.StatusOK, Response{StatusCode: 1, StatusMsg: "User doesn't exist"})
-		return
-	} else {
-		if uid != userIdStr {
-			c.JSON(http.StatusOK, Response{StatusCode: 1, StatusMsg: "User doesn't approved"})
-			return
-		}
-	}
 	switch actionType {
 	case 1:
-
+		err := service.Like(userId, videoId)
+		if err != nil {
+			c.JSON(http.StatusOK, Response{StatusCode: 1, StatusMsg: err.Error()})
+		} else {
+			c.JSON(http.StatusOK, Response{StatusCode: 0, StatusMsg: "Like success"})
+		}
 	case 2:
-
+		err := service.Unlike(userId, videoId)
+		if err != nil {
+			c.JSON(http.StatusOK, Response{StatusCode: 1, StatusMsg: err.Error()})
+		} else {
+			c.JSON(http.StatusOK, Response{StatusCode: 0, StatusMsg: "Like success"})
+		}
+	default:
+		c.JSON(http.StatusOK, Response{StatusCode: 1, StatusMsg: "parameter error"})
 	}
 }
 
 // FavoriteList all users have same favorite video list
 func FavoriteList(c *gin.Context) {
-	c.JSON(http.StatusOK, VideoListResponse{
-		Response: Response{
-			StatusCode: 0,
-		},
-		VideoList: DemoVideos,
+	// userIdStr := c.GetString(ctxUidKey)
+	userIdStr := c.Query("user_id")
+	userId, _ := strconv.ParseInt(userIdStr, 10, 64)
+	videoList, _ := service.VideoList(userId)
+	c.JSON(http.StatusOK, FavoriteListResponse{
+		Response:  Response{StatusCode: 0},
+		VideoList: videoList,
 	})
 }
