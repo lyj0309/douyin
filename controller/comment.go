137f58fdb21227ac3b1efb79154debc2ec32d975
ChenPeng-1 <cp1756207655@gmail.com>
2022-06-11 11:45:30 +0800

    评论部分增加参数校验

--- controller/comment.go
+++ controller/comment.go
@@ -24,6 +24,10 @@ func CommentAction(c *gin.Context) {
 	actionType := c.Query("action_type")
 	if actionType == "1" {
 		commentText := c.Query("comment_text")
+		if len(commentText) == 0 {
+			c.JSON(http.StatusOK, Response{StatusCode: 1, StatusMsg: "评论内容不能为空"})
+			return
+		}
 		commentVo, err := service.CommentAdd(userId, videoId, commentText)
 		if err != nil {
 			c.JSON(http.StatusOK, Response{StatusCode: 1, StatusMsg: err.Error()})
@@ -50,8 +54,17 @@ func CommentAction(c *gin.Context) {
 func CommentList(c *gin.Context) {
 	userIdStr := c.GetString("user_id")
 	videoIdStr := c.Query("video_id")
-	videoId, _ := strconv.ParseInt(videoIdStr, 10, 64)
-	commentList, _ := service.CommentList(videoId, userIdStr)
+	videoId, err := strconv.ParseInt(videoIdStr, 10, 64)
+	if err != nil {
+		c.JSON(http.StatusOK, Response{StatusCode: 1, StatusMsg: "参数有误"})
+		return
+	}
+	var commentList []service.Comment
+	commentList, err = service.CommentList(videoId, userIdStr)
+	if err != nil {
+		c.JSON(http.StatusOK, Response{StatusCode: 1, StatusMsg: err.Error()})
+		return
+	}
 	c.JSON(http.StatusOK, CommentListResponse{
 		Response:    Response{StatusCode: 0},
 		CommentList: commentList,
